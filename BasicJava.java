b8af3bc57ca36b2ea23a8dfe46afcc532adbb00f
berlianristy <91619500+berlianristy@users.noreply.github.com>
2021-10-12 20:06:07 +0700

    java-challenge-1
    challenge-1

---
+++ BasicJava.java
@@ -0,0 +1,38 @@
+public class BasicJava {
+    public static void main(String[] args) {
+    
+    // variable
+    String first, last, adress,phoneNumber, hobby;
+    int age;
+    double weight, height;
+    char bloodType;
+    float distance;
+    boolean online;
+
+    // number
+    first = "Berlian";
+    last = "Risty";
+    adress = "Medan";
+    phoneNumber = "087708338298";
+    hobby = "singing";
+    age = 21;
+    weight = 55;
+    height = 156;
+    online = true;
+    distance = 9f;
+    bloodType = 'O';
+
+
+    // output
+    System.out.println("Name : " + first + last);
+    System.out.println("Adress : " + adress);
+    System.out.println("Phone Number : " + phoneNumber);
+    System.out.println("Hobby : " + hobby);
+    System.out.println("Age : " + age);
+    System.out.println("Weight : " + weight + " kg");
+    System.out.println("Height : " + height + " cm");
+    System.out.println("Distance : " + distance + " km");
+    System.out.println("Blood type : " + bloodType);
+    System.out.println("Online : " + online);
+    }
+}
